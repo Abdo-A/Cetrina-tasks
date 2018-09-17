c238c46433a3f463f542b49d01fbcdde0f7f4fac
Abdo-A <abdopoetry@gmail.com>
2018-09-17 14:58:06 +0200

    Replaced semantic ui with antd

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -1,4 +1,4 @@
-import { Menu, Sidebar, Button, Icon } from "semantic-ui-react";
+import { Drawer, Icon } from "antd";
 import React from "react";
 
 import "./MainMenu.css";
@@ -7,41 +7,23 @@ const MainMenu = props => {
   return (
     <div className="MainMenu">
       <Icon
-        onClick={props.toggleSideBar}
-        name="content"
+        type="menu-fold"
+        theme="outlined"
         className="MainMenu__ToggleIcon"
-        color="grey"
-        size="large"
-        inverted
+        onClick={props.toggleSideBar}
+        style={{ color: "white" }}
       />
 
-      <Sidebar
-        as={Menu}
-        animation="overlay"
-        icon="labeled"
-        inverted
-        //onHide={props.handleSidebarHide}
-        vertical
+      <Drawer
+        placement="right"
+        closable
+        onClose={props.toggleSideBar}
         visible={props.sideBarVisible}
-        width="thin"
-        direction="right"
       >
-        <br />
-        <Icon
-          onClick={props.toggleSideBar}
-          name="close"
-          style={{
-            cursor: "pointer",
-            marginLeft: "70%"
-          }}
-          color="grey"
-          inverted
-        />
-
-        <Menu.Item as="a">Home</Menu.Item>
-        <Menu.Item as="a">Games</Menu.Item>
-        <Menu.Item as="a">Channels</Menu.Item>
-      </Sidebar>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
     </div>
   );
 };
